8bb26293e171d5211b581151796b6647fa92f83a
Aurélien <aurelien.strimelle@bstorm.be>
2022-08-23 14:07:44 +0200

    Add logout feature

--- src/containers/connection-button/connection-button.jsx
+++ src/containers/connection-button/connection-button.jsx
@@ -1,22 +1,36 @@
 import { Button, ButtonGroup } from '@mui/material';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { logoutUser } from '../../store/actions/auth-action';
 
 const ConnectionButton = () => {
 
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const isConnected = useSelector(state => state.auth.isConnected);
+
+    const handleLogout = () => {
+        dispatch(logoutUser());
+        navigate("/");
+    };
+
     return (
         <ButtonGroup>
-            <Button variant='contained' color='success'
-                component={NavLink} to='/auth/login'>
-                Login
-            </Button>
-            <Button variant='contained' color='warning'
-                component={NavLink} to='/auth/register'>
-                Register
-            </Button>
-            <Button variant='contained' color='error'
-                component={NavLink} to='/'>
-                Logout
-            </Button>
+            {!isConnected ? (<>
+                <Button variant='contained' color='success'
+                    component={NavLink} to='/auth/login'>
+                    Login
+                </Button>
+                <Button variant='contained' color='warning'
+                    component={NavLink} to='/auth/register'>
+                    Register
+                </Button>
+            </>) : (<>
+                <Button variant='contained' color='error'
+                    onClick={handleLogout} >
+                    Logout
+                </Button>
+            </>)}
         </ButtonGroup>
     );
 };
