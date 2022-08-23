d617060ca8f09be25cf4802c394bdf3974260058
Aurélien <aurelien.strimelle@bstorm.be>
2022-08-23 14:24:31 +0200

    Cleanup

--- src/containers/header/header.jsx
+++ src/containers/header/header.jsx
@@ -1,34 +1,32 @@
-import { AppBar, Button, ButtonGroup, IconButton, Toolbar, Typography } from '@mui/material';
+import { AppBar, IconButton, Toolbar, Typography } from '@mui/material';
 import MenuIcon from '@mui/icons-material/MenuTwoTone';
 import ConnectionButton from '../connection-button/connection-button';
 
-const Header = ({ onOpenMenu }) => {
+const Header = ({ onOpenMenu }) => (
+    <header>
+        <AppBar position='static' color='primary'>
+            <Toolbar>
+                <IconButton
+                    onClick={() => onOpenMenu()}
+                    size='large'
+                    edge='start'
+                    color='inherit'
+                    aria-label='menu'
+                    sx={{ mr: 2 }}>
+                    <MenuIcon />
+                </IconButton>
+                <Typography
+                    component='div'
+                    variant='h5'
+                    sx={{ flexGrow: 1 }}>
+                    Task List
+                </Typography>
 
-    return (
-        <header>
-            <AppBar position='static' color='primary'>
-                <Toolbar>
-                    <IconButton
-                        onClick={() => onOpenMenu()}
-                        size='large'
-                        edge='start'
-                        color='inherit'
-                        aria-label='menu'
-                        sx={{ mr: 2 }}>
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography
-                        component='div'
-                        variant='h5'
-                        sx={{ flexGrow: 1 }}>
-                        Task List
-                    </Typography>
+                <ConnectionButton />
+            </Toolbar>
+        </AppBar>
+    </header>
+);
 
-                    <ConnectionButton />
-                </Toolbar>
-            </AppBar>
-        </header>
-    );
-};
 
 export default Header;
